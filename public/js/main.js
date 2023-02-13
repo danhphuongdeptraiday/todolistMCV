324e39a4b68feaad0c3469cc88cab78012bf937b
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-02-13 21:54:19 +0700

    new commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,25 @@
-const handleCreateTask = () => {
-  //e nhìn n post cái user lên không mà ở controller thì
-  fetch("http://localhost:3000/task/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      user: {
-        name: "John",
-        email: "john@example.com",
-      },
-    }),
-  });
-};
+// const handleCreateTask = () => {
+//   //e nhìn n post cái user lên không mà ở controller thì
+//   fetch("http://localhost:3000/task/create", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       user: {
+//         name: "John",
+//         email: "john@example.com",
+//       },
+//     }),
+//   });
+// };
+
+let toggleBtn = document.querySelector(".navbar-toggler");
+let navbarSupportedContent = document.querySelector("#navbarSupportedContent");
+toggleBtn.addEventListener("click", () => {
+  if (navbarSupportedContent.style.display == "none") {
+    navbarSupportedContent.style.display = "block";
+  } else {
+    navbarSupportedContent.style.display = "none";
+  }
+});
